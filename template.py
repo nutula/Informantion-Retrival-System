83bb43c207f47590c5e87c2fee3ab7153fc17881
praveennutulapati <122986521+praveennutulapati@users.noreply.github.com>
2025-05-21 09:58:58 -0500

    folder-str-added

--- template.py
+++ template.py
@@ -2,28 +2,34 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(Level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
 list_of_files = [
     "src/__init__.py",
     "src/helper.py",
-    ".env"
+    ".env",
     "requirements.txt",
-    "setup.py"
-    "app.py"
-    "research/trails.ipynb",
+    "setup.py",
+    "app.py",
+    "research/trials.ipynb",
+    "test.py"
+
 ]
+
+
 
 for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
 
-    if filedir !="":
-        os.makedirs(filedir, exist_ok = True)
-        logging.info(f"Creating directory: {filedir} for the file: {filename}")
 
-    if (not os.path.exists(filepath) or (os.path.getsize(filepath) == 0)):
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
             pass
             logging.info(f"Creating empty file: {filepath}")
