8e8121b4f62bc646fa752ca8a44c2230eefad9d5
praveennutulapati <122986521+praveennutulapati@users.noreply.github.com>
2025-05-22 15:19:52 -0500

    requirements-added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Information retriver',
+    version='0.0.0',
+    author='Praveen',
+    author_email='nutulapatipraveen7@gmail.com',
+    packages=find_packages(),
+    install_requires = []
+)
